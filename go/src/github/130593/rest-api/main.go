24007bf4e558544cff1638e04d38ae19d507a4bc
Guilherme <guilherme.silva.ext@sascar.com.br>
2019-10-14 20:25:56 -0300

    add GO Project

---
+++ go/src/github/130593/rest-api/main.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+func main() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/hello", SayHello)
+	http.ListenAndServe("0.0.0.0:8080", handler)
+}
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Hello world`)
+}
